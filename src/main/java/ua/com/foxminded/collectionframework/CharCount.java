e671f3a347d68cd85c603f926d6c706994f0c35c
foxstudent103367 <galibrodaa@gmail.com>
2023-02-11 21:26:30 +0200

    CharCount - value class to save count of chars

---
+++ src/main/java/ua/com/foxminded/collectionframework/CharCount.java
@@ -0,0 +1,29 @@
+package ua.com.foxminded.collectionframework;
+
+public class CharCount {
+
+    char character;
+    int count;
+
+    public CharCount(char character, int count) {
+        this.character = character;
+        this.count = count;
+    }
+    
+    public char getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+}
