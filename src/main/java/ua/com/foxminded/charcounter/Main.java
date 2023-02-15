a475728e34d48b166b3db619595516dfc2920430
foxstudent103367 <galibrodaa@gmail.com>
2023-02-15 08:23:41 +0200

    format() and printCharCount() moved to main() and deleted

--- src/main/java/ua/com/foxminded/charcounter/Main.java
+++ src/main/java/ua/com/foxminded/charcounter/Main.java
@@ -17,51 +17,27 @@ public class Main {
         System.out.println("Enter 'Stop' to stop program");
         String text = readLine();
         while (!text.equals("Stop")) {
-            printCharCount(text);
+            if (!cacheTest.getData().containsKey(text)) {
+                cacheTest.addData(text);
+            }
+            for (Character character : cacheTest.getData().get(text).keySet()) {
+                if (character.equals('"')) {
+                    System.out.println(APOSTROPHE + character.toString() + APOSTROPHE + HYPHEN
+                            + cacheTest.getData().get(text).get(character) + NEW_LINE);
+                } else {
+                    System.out.println(QUOTES + character.toString() + QUOTES + HYPHEN
+                            + cacheTest.getData().get(text).get(character) + NEW_LINE);
+                }
+            }
             text = readLine();
         }
         scanner.close();
-    }
-
-    private static void printCharCount(String text) {
-        if (cacheTest.getData().containsKey(text)) {
-            System.out.println(format(text));
-        } else {
-            cacheTest.addData(text);
-            System.out.println(format(text));
-        }
     }
 
     private static String readLine() {
         String text;
         text = scanner.next();
         return text;
-    }
-
-    private static String format(String text) {
-        if (text == null) {
-            throw new NullPointerException("Text cann`t be null");
-        } else if (cacheTest == null) {
-            throw new NullPointerException("Cache cann`t be null");
-        }
-        if (text.equals("")) {
-            return "#NO TEXT ENTERED#" + NEW_LINE;
-        }
-        StringBuilder formattedResult = new StringBuilder();
-        try {
-            for (Character character : cacheTest.getData().get(text).keySet()) {
-                if (character.equals('"')) {
-                    formattedResult.append(APOSTROPHE + character.toString() + APOSTROPHE + HYPHEN
-                            + cacheTest.getData().get(text).get(character) + NEW_LINE);
-                } else {
-                    formattedResult.append(QUOTES + character.toString() + QUOTES + HYPHEN
-                            + cacheTest.getData().get(text).get(character) + NEW_LINE);
-                }
-            }
-        } catch (NullPointerException e) {
-            throw new NullPointerException("No text in cache");
-        }
-        return formattedResult.toString();
     }
 
 }
