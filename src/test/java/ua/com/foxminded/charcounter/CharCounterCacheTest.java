175c7235295a325033f3d289baad22f8587db2df
foxstudent103367 <galibrodaa@gmail.com>
2023-02-23 08:07:44 +0200

    Mockito for test

--- src/test/java/ua/com/foxminded/charcounter/CharCounterCacheTest.java
+++ src/test/java/ua/com/foxminded/charcounter/CharCounterCacheTest.java
@@ -2,31 +2,41 @@ package ua.com.foxminded.charcounter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.Mockito.*;
 import org.junit.jupiter.api.Test;
 
 class CharCounterCacheTest {
 
-    private CharCounter charCounter = new UniqueCharCounter();
-    private CharCounterCache charCounterCache = new CharCounterCache(charCounter);
+    private CharCounter charCounter = mock(CharCounter.class);
+    private CharCounter cachedCharCounter = new CharCounterCache(charCounter);
 
     @Test
-    void countCharacters_sameInput_returnsCachedResult() {
-        String input = "abc";
-        Map<Character, Integer> actualResult1 = charCounterCache.countCharacters(input);
-        Map<Character, Integer> actualResult2 = charCounterCache.countCharacters(input);
+    void countCharacters_input_methodFromCharCounterCalledOnce() {
+        String text = "Heeeeello, Emy!!";
 
-        assertEquals(actualResult1, actualResult2);
-    }
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('H', 1);
+        expected.put('e', 5);
+        expected.put('E', 1);
+        expected.put('l', 2);
+        expected.put('o', 1);
+        expected.put(',', 1);
+        expected.put(' ', 1);
+        expected.put('m', 1);
+        expected.put('y', 1);
+        expected.put('!', 2);
 
-    @Test
-    void countCharacters_differentInput_returnsDifferentResult() {
-        String input1 = "abc";
-        String input2 = "def";
-        Map<Character, Integer> result1 = charCounterCache.countCharacters(input1);
-        Map<Character, Integer> result2 = charCounterCache.countCharacters(input2);
+        when(charCounter.countCharacters(text)).thenReturn(expected);
 
-        assertNotSame(result1, result2);
+        Map<Character, Integer> actual1 = cachedCharCounter.countCharacters(text);
+        Map<Character, Integer> actual2 = cachedCharCounter.countCharacters(text);
+        
+        assertEquals(expected, actual1);
+        assertEquals(actual1, actual2);
+
+        verify(charCounter, times(1)).countCharacters(text);
     }
 }
