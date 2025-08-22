bf8292ce5e8182e223bf0e62949010a56d25303b
Halibroda <galibrodaa@gmail.com>
2025-08-22 17:23:28 +0300

    missing files

---
+++ src/main/java/ua/com/foxminded/charcounter/CachedCharCounter.java
@@ -0,0 +1,18 @@
+package ua.com.foxminded.charcounter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CachedCharCounter extends CharCounterDecorator {
+
+    private Map<String, Map<Character, Integer>> cache = new HashMap<>();
+
+    public CachedCharCounter(CharCounter charCounter) {
+        super(charCounter);
+    }
+
+    @Override
+    public Map<Character, Integer> countCharacters(String text) {
+        return cache.computeIfAbsent(text, super::countCharacters);
+    }
+}
---
+++ src/test/java/ua/com/foxminded/charcounter/CachedCharCounterTest.java
@@ -0,0 +1,50 @@
+package ua.com.foxminded.charcounter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class CachedCharCounterTest {
+
+    @Mock
+    private CharCounter charCounter;
+
+    @InjectMocks
+    private CachedCharCounter cachedCharCounter;
+
+    @Test
+    void countCharacters_input_methodFromCharCounterCalledOnce() {
+        MockitoAnnotations.openMocks(this);
+
+        String text = "Heeeeello, Emy!!";
+
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('H', 1);
+        expected.put('e', 5);
+        expected.put('E', 1);
+        expected.put('l', 2);
+        expected.put('o', 1);
+        expected.put(',', 1);
+        expected.put(' ', 1);
+        expected.put('m', 1);
+        expected.put('y', 1);
+        expected.put('!', 2);
+
+        when(charCounter.countCharacters(text)).thenReturn(expected);
+
+        Map<Character, Integer> actual1 = cachedCharCounter.countCharacters(text);
+        Map<Character, Integer> actual2 = cachedCharCounter.countCharacters(text);
+
+        assertEquals(expected, actual1);
+        assertEquals(actual1, actual2);
+
+        verify(charCounter, times(1)).countCharacters(text);
+    }
+}
